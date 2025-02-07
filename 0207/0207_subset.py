f5886b783dfe0fc0a22b41c470ac92d361f97488
권예경 <qwer4359@naver.com>
2025-02-07 16:40:59 +0900

    subset

--- 0207/0207_subset.py
+++ 0207/0207_subset.py
@@ -1,10 +1,16 @@
-#
-# arr = range(1,13)
-# T = int(input())
-# for tc in range(1, T+1):
-#     N, K = map(int, input().split())
-#     count = 0
-#     for subset in range(1<<3):
-#         for j in range(12):
-#             if i & (1<<j):
-#                 if sum(arr[j]) ==
+T = int(input())
+arr = range(1,13)
+for tc in range(1, T+1):
+    N, K = map(int, input().split())
+    result = 0
+    for i in range(1<<12):
+        subset = []
+        for j in range(12):
+            if i & (1<<j):
+                if :
+                    subset.append(arr[j])
+
+    print(f'#{tc} {subset}')
+
+
+    # print(f'#{tc} {count}')
