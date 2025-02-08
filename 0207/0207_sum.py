917452a198dc8b9ec5f8469ab79767c457a9b8bc
GYG <qwer4359@naver.com>
2025-02-08 16:47:34 +0900

    sum

--- 0207/0207_sum.py
+++ 0207/0207_sum.py
@@ -1,31 +1,40 @@
-for tc in range(1,11):
+# 두 개의 값을 받아 큰 값을 출력하는 함수
+def who_is_max(val1, val2):
+    if val1 > val2:
+        return val1
+    else:
+        return val2
+
+# 리스트를 받아 합을 출력하는 함수
+def sumsum(lst):
+    result = 0
+    for i in range(100):
+        result += lst[i]
+    return result
+
+for tc in range(1, 11):
     no = int(input())
-    arr = [list(map(int, input().split())) for _ in range(100)]
+    arr = [list(map(int,input().split())) for _ in range(100)]
+        
+    # 초기 최댓값으로 대각선 합을 부여
+    max_val = 0
+    for n in range(100):
+        max_val += arr[n][n]
 
-    max_sum = 0
-    for r in range(100):
-        sumsum = 0
-        for c in range(100):
-            sumsum += arr[r][c]
-        if sumsum > max_sum:
-            max_sum = sumsum
+    # 역대각선 합과 최댓값 비교
+    sum_val = 0
+    for n in range(100):
+        sum_val += arr[99-n][n]
+    max_val = who_is_max(max_val, sum_val)
 
-        sumsum = 0
-        for c in range(100):
-            sumsum += arr[c][r]
-        if sumsum > max_sum:
-            max_sum = sumsum
+    # 행 기준 합과 최댓값 비교
+    for r in arr:
+        row_sum = sumsum(r)
+        max_val = who_is_max(max_val, row_sum)
 
-        sumsum = 0
-        sumsum += arr[r][r]
-        if sumsum > max_sum:
-            max_sum = sumsum
+    # 열 기준 합과 최댓값 비교
+    for arr_1 in zip(*arr):
+        col_sum = sumsum(arr_1)
+        max_val = who_is_max(max_val, col_sum)
 
-        sumsum = 0
-        sumsum += arr[r][99 - r]
-        if sumsum > max_sum:
-            max_sum = sumsum
-
-    print(f'#{tc} {max_sum}')
-    
-    # 다시도전
+    print(f'#{tc}', max_val)
