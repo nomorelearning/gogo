223cda297e1e72575b8be20de22d422898a080c3
GYG <qwer4359@naver.com>
2025-02-08 15:22:57 +0900

    zigzag

--- 0207/0207_zigzag.py
+++ 0207/0207_zigzag.py
@@ -1,12 +1,15 @@
-arr = [[0] * 10 for _ in range(10)]
-row, col, w, h = map(int, input().split())
+T = int(input())
+for tc in range(1, T+1):
+    arr = [[0] * 10 for _ in range(10)]
+    row, col, w = map(int, input().split())
 
-num = 1
-for r in range(row, row + h):
-    for c in range(col, col + w):
-        n = r - row
-        arr[r][c + (col + w - 1 -2 * c)*(n % 2)] = num
-        num += 1
-
-for lst in arr:
-    print(*lst)
+    num = 1
+    for r in range(row, row + w):
+        for c in range(w):
+            n = r - row
+            arr[r][col + c + (w - 1 - 2 * c)*(n % 2)] = num
+            num += 1
+    
+    print(f'#{tc}')
+    for lst in arr:
+        print(*lst)
