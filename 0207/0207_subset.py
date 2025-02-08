3f06a5884cbd0eaeba6515a00c4fc0c6d8370743
GYG <qwer4359@naver.com>
2025-02-08 14:44:43 +0900

    finally

--- 0207/0207_subset.py
+++ 0207/0207_subset.py
@@ -1,17 +1,16 @@
 T = int(input())
 arr = range(1,13)
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N, K = map(int, input().split())
     result = 0
     for i in range(1<<12):
-        subset = []
+        subset_sum = 0
+        subset_count = 0
         for j in range(12):
             if i & (1<<j):
-                subset.append(arr[j])
-        if K in subset:
-            result = subset.count(K)
-
+                subset_sum += arr[j]
+                subset_count += 1
+                
+        if subset_sum == K and subset_count == N:
+            result += 1
     print(f'#{tc} {result}')
-
-
-    # print(f'#{tc} {count}')
