3c23c1060988fd9027c5462bf908e37fb365f345
GYG <qwer4359@naver.com>
2025-02-08 22:47:37 +0900

    view

--- 0205/0205_view.py
+++ 0205/0205_view.py
@@ -1,3 +1,30 @@
-data = list(map(int,input().split()))
+def is_this_max(list_1):        # 가운데 값이 최댓값인지 확인
+    x = list_1[2]
+    for k in list_1:
+        if k > x:
+            return False
+    return True
 
-print(data)
+def how_floor(list_2):          # 두번째 큰 값과 차이를 계산
+    second = 0
+    for l in [0, 1, 3, 4]:
+        if list_2[l] > second:
+            second = list_2[l]
+    return second
+
+for tc in range(1, 11):
+    n = int(input())
+    lst = list(map(int,input().split()))
+
+    result = 0
+    for i in range(2, n-2):
+        list_now = lst[i-2:i+3]         # i를 중앙 인덱스로 하는 길이 5인 리스트 임시 생성
+        if is_this_max(list_now):       # 가운데가 최댓값인지 확인 후 차이 계산산
+            second = how_floor(list_now)
+            result += list_now[2] - second      # 차이를 누적하여 result 만들기기
+        else:
+            continue
+
+    print(f'#{tc}', result)
+        
+        
