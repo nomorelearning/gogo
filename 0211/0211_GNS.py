4a1f663167aa542a9f1f2a62845ab5c6e6e6192d
GYG <qwer4359@naver.com>
2025-02-11 23:50:53 +0900

    night

--- 0211/0211_GNS.py
+++ 0211/0211_GNS.py
@@ -1,26 +1,36 @@
 import sys
 sys.stdin = open('test.txt', 'r')
 
-which = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
+num_dict = {
+    "ZRO" : 0,
+    "ONE" : 1,
+    "TWO" : 2,
+    "THR" : 3,
+    "FOR" : 4,
+    "FIV" : 5,
+    "SIX" : 6,
+    "SVN" : 7,
+    "EGT" : 8,
+    "NIN" : 9
+}
 T = int(input())
 
-def mk_lst(n, tuple, texts):
-    lst = [0] * 10
-    for i in range(n):
-        for j in range(10):
-            if texts[i] == tuple[j]:
-                lst[j] += 1
-                break
-    return lst
+
 
 for _ in range(T):
     tc, n = input().split()
     texts = list(input().split())
     n = int(n)
+    
+    count = [0] * 10
+    for i in range(n):
+        count[num_dict[texts[i]]] += 1
+        
+    print(count)
     result = []
-    lst = mk_lst(n, which, texts)
-    for i in range(10):
-        for _ in range(lst[i]):
-            result.append(which[i])
+    for eng_num in num_dict:
+        for _ in range(count[num_dict[eng_num]]):
+            result.append(eng_num)
+    
     print(tc)
     print(*result)
